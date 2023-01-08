d590d813d1e0d029a37978526c44302c6f46974c
Abhishek <abhishek.kshattriye@credenc.com>
2023-01-09 01:52:53 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from '@mui/material'
+import { Form, Formik } from 'formik'
+import React from 'react'
+import * as yup from 'yup'
+
+const validationSchema = yup.object ({
+name: yup.string().required('Name is required')
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Formik initialValues = {{
+      name:""
+    }}
+    onSubmit={values=>{
+      alert(JSON.stringify(values, null, 2))
+    }}
+    validationSchema = {validationSchema}>
+      {(formik) =>(
+    <Form onSubmit={formik.handleSubmit}>
+      <TextField 
+      label='Name'
+      id='name'
+      name='name'
+      value={formik.values.name}
+      onChange = {formik.handleChange}
+      error = {formik.touched.name && Boolean(formik.errors.name)}
+      helperText = {formik.touched.name && formik.errors.name}
+        />
+
+        <Button type='submit' color='primary' variant='contained'> Submit </Button>
+      
+      </Form>
+    )}
+      </Formik>
+  )
 }
 
-export default App;
+export default App
